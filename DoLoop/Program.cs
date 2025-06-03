f1ef7998ef8e48e32ba02958487ffbf76315955a
mgapp <mgapp@MIKEGYO>
2025-06-03 18:14:26 -0400

    Add project files.

---
+++ DoLoop/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace DoLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isValid;
+            int age;
+            int testNumber = 0;
+            do
+            {
+                Console.WriteLine(testNumber);
+                testNumber += 3;
+            }
+            while (testNumber < 10);
+            //do
+            //{
+            //    Console.WriteLine("What is your age: ");
+            //    string ageText = Console.ReadLine();
+
+            //    isValid = int.TryParse(ageText, out age);
+
+            //    if (isValid == false)
+            //    {
+            //        Console.WriteLine("That was an invalid age");
+            //    }
+            //} while (isValid == false);
+
+            //Console.WriteLine($"Your age is {age}");
+
+            //while (isValid == false)
+            //{
+
+            //}
+        }
+    }
+}
